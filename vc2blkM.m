cc22c1062ec2390e8ebc078ed89126fe93c31ed8
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2019-10-17 15:47:27 -0400

    Update vc2blkM.m

--- vc2blkM.m
+++ vc2blkM.m
@@ -1,31 +1,25 @@
-function M = vc2blkM(vc, r, height, width) 
-%vc2blkM Reshaping a matrix vc of rc by 1 vectors into a block-matrix M of rM by cM size 
-% Each rc-element column of vc is converted into a r by c  block of a matrix M and placed as a block-row element 
-
-[featureVectorsSize ,featureVectorCount] = size(vc) ; 
-pxls = featureVectorsSize*featureVectorCount ; 
-
-if ( (rem(pxls, height) ~= 0) || (rem(height, r) ~= 0) ) 
-    error('incorrect number of rows of the matrix') 
-end
-
-if ( (rem(pxls, width) ~= 0) || (rem(width, r) ~= 0) ) 
-    error('incorrect number of rows of the matrix') 
-end
-
-
-cM = pxls/height; 
-
-if ( (rem(featureVectorsSize, r) ~= 0) || (rem(featureVectorCount*r, height) ~= 0) ) 
-error('incorrect block size') 
+function X = vec2blkM(vec, r, h, w)  
+%vec2blkM reshapes a matrix vec of rc by 1 vectors into a block-matrix X of xh by ph size  
+% Each rc-element column of vec is converted into a r by c block of a matrix X and placed as a 
+block-row element   
+[VectorsSize ,VectorCount] = size(vec) ;  
+73 
+px = VectorsSize*VectorCount ;  
+if ( (rem(px, h) ~= 0) || (rem(h, r) ~= 0) )  
+    error('number of rows of the matrix error')  
 end 
-c = featureVectorsSize/r ; 
-xM = zeros(r, featureVectorCount*c); 
-xM(:) = vc ; 
-nrb = height/r ; 
-M = zeros(height, cM); 
-for ii = 0:nrb-1 
-M((1:r)+ii*r, :) = xM(:, (1:cM)+ii*cM) ; 
+if ( (rem(px, w) ~= 0) || (rem(w, r) ~= 0) )  
+    error('number of rows of the matrix erro')  
 end 
-M=M*255; 
-M= uint8(M);
+ph = px/h;  
+if ( (rem(VectorsSize, r) ~= 0) || (rem(VectorCount*r, h) ~= 0) )  
+error('block size error')  
+end  
+c = VectorsSize/r ;  
+xh = zeros(r, VectorCount*c);  
+xh(:) = vec ;  
+nrb = h/r ;  
+X = zeros(h, ph);  
+for ii = 0:nrb-1  
+X((1:r)+ii*r, :) = xh(:, (1:ph)+ii*ph) ;  
+end
