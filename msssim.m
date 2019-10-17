fc32410c98eb44f705d21b033a88ed8286ef65d5
Bhavik Bhoir <43330221+bhavikbhoir@users.noreply.github.com>
2019-10-17 15:57:39 -0400

    Update msssim.m

--- msssim.m
+++ msssim.m
@@ -1,107 +1,60 @@
-function overall_mssim = msssim(im1, im2, K, window, level, weight, method)
+function mssim = msssim(X, Xcomp, K, window, scale, weight, method) 
 
-% Multi-scale Structural Similarity Index (MS-SSIM)
-% Z. Wang, E. P. Simoncelli and A. C. Bovik, "Multi-scale structural similarity
-% for image quality assessment," Invited Paper, IEEE Asilomar Conference on
-% Signals, Systems and Computers, Nov. 2003
+K = [0.01 0.03]; 
+window = fspecial('gaussian', 11, 1.5); 
+scale = 5; 
+weight = [0.1448 0.2856 0.2001 0.2363 0.1333]; 
+method = 'product'; 
+[H1 W1] = size(win); 
+min_img_width = min(H, W)/(2^(scale-1)); 
+max_win_width = max(H1, W1); 
+ 
+im1 = double(X); im2 = double(Xcomp); 
 
-if (nargin < 2 || nargin > 7)
-   overall_mssim = -Inf;
-   return;
-end
+C1 = (K(1)*L)^2; 
+C2 = (K(2)*L)^2; 
 
-if (~exist('K'))
-   K = [0.01 0.03];
-end
+win = window/sum(sum(window)); 
+mu1   = filter2(win, im1, 'valid'); 
+mu2   = filter2(win, im2, 'valid'); 
 
-if (~exist('win'))
-   window = fspecial('gaussian', 11, 1.5);
-end
+mu1_sq = mu1.*mu1; 
+mu2_sq = mu2.*mu2; 
+mu1_mu2 = mu1.*mu2; 
 
-if (~exist('level'))
-   level = 3;
-end
+sigma1_sq = filter2(win, im1.*im1, 'valid') - mu1_sq; 
+sigma2_sq = filter2(win, im2.*im2, 'valid') - mu2_sq; 
+sigma12 = filter2(win, im1.*im2, 'valid') - mu1_mu2; 
 
-if (~exist('weight'))
-%    weight = [0.0448 0.2856 0.3001 0.2363 0.1333];
-weight = [0.6449 0.1886 0.1666];
-% weight = [1 0 0];
+num1 = 2*mu1_mu2 + C1; 
+num2 = 2*sigma12 + C2; 
 
-end
+den1 = mu1_sq + mu2_sq + C1; 
+den2 = sigma1_sq + sigma2_sq + C2; 
 
-if (~exist('method'))
-   method = 'product';
-end
+ssim_map = ones(size(mu1)); 
+index = (denominator1.*denominator2 > 0); 
+ssim_map(index) =   (num1(index).*num2(index))./(den1(index).*den2(index)); 
+index = (den1 ~= 0) & (den2 == 0); 
+ssim_map(index) = num1(index)./den1(index); 
+ 
+ssim = mean2(ssim_map); 
+downsamplefilter = ones(2)./4; 
 
-if (size(im1) ~= size(im2))
-   overall_mssim = -Inf;
-   return;
-end
-
-[M N] = size(im1);
-if ((M < 11) || (N < 11))
-   overall_mssim = -Inf;
-   return
-end
-
-if (length(K) ~= 2)
-   overall_mssim = -Inf;
-   return;
-end
-
-if (K(1) < 0 || K(2) < 0)
-   overall_mssim = -Inf;
-   return;
-end
-  
-[H W] = size(window);
-
-if ((H*W)<4 || (H>M) || (W>N))
-   overall_mssim = -Inf;
-   return;
-end
+for l = 1:scale 
+   [mssim_array(l) ssim_map_array{l} mcs_array(l) cs_map_array{l}] = ssim(im1, im2, K, 
+   window); 
    
-if (level < 1)
-   overall_mssim = -Inf;
-   return
-end
+   filtered_im1 = imfilter(im1, downsamplefilter, 'symmetric', 'same'); 
+   filtered_im2 = imfilter(im2, downsamplefilter, 'symmetric', 'same'); 
+   
+   im1 = filtered_im1(1:2:end, 1:2:end); 
+   im2 = filtered_im2(1:2:end, 1:2:end); 
+end 
 
-
-min_img_width = min(M, N)/(2^(level-1));
-max_win_width = max(H, W);
-if (min_img_width < max_win_width)
-   overall_mssim = -Inf;
-   return;
-end
-
-if (length(weight) ~= level || sum(weight) == 0)
-   overall_mssim = -Inf;
-   return;
-end
-
-if (method ~= 'wtd_sum' & method ~= 'product')
-   overall_mssim = -Inf;
-   return;
-end
-
-% downsample_filter = ones(2)./4;
-downsample_filter = [1,1,1,1]./4;
-im1 = double(im1);
-im2 = double(im2);
-for l = 1:level
-   [mssim_array(l) ssim_map_array{l} mcs_array(l) cs_map_array{l}] = ssim_index_new_720(im1, im2, K, window);
-%   [M N] = size(im1);
-   filtered_im1 = imfilter(im1, downsample_filter, 'symmetric', 'same');
-   filtered_im2 = imfilter(im2, downsample_filter, 'symmetric', 'same');
-%    clear im1, im2;
-   im1 = filtered_im1(1:2:end, 1:2:end);
-   im2 = filtered_im2(1:2:end, 1:2:end);
-end
-
-if (method == 'product')
-%   overall_mssim = prod(mssim_array.^weight);
-   overall_mssim = prod(mcs_array(1:level-1).^weight(1:level-1))*(mssim_array(level).^weight(level));
-else
-   weight = weight./sum(weight);
-   overall_mssim = sum(mcs_array(1:level-1).*weight(1:level-1)) + mssim_array(level).*weight(level);
+if (method == 'product')    
+   mssim = prod(mcs_array(1:scale-1).^weight(1:scale-1))*(mssim_array(scale).^weight(scale)); 
+else 
+   weight = weight./sum(weight); 
+   mssim = sum(mcs_array(1:scale-1).*weight(1:scale-1)) + mssim_array(scale).*weight(scale);
 end
